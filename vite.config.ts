289167e13fb1594271413163dc18b7e782d9307a
Gabriel Vargas <gabrielfvf@outlook.com>
2026-01-15 04:17:51 -0300

    fix: Corrige config do vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/gabrielfvf7.github.io/' : '/',
-}))
+  base: '/',
+})
